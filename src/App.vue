fdae3c6f3b20eb2bd576f60aa1a6c81697541385
r567tw <r567twjob@gmail.com>
2024-11-20 13:40:58 +0800

    First Version

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import Form from './components/Form.vue'
 
 <template>
   <main>
-    <h4>编辑 <code>src/App.vue</code> 并保存以重新加载</h4>
+    <h4>台灣勞工勞健保計算機</h4>
     <Form />
   </main>
 </template>
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -8,7 +8,7 @@
     ElSelect,
     ElOption,
   } from 'element-plus';
-  import laborInsuranceRanges from '@/data/insurance-ranges'; // 確保路徑正確
+  import laborInsuranceRanges from '@/data/insurance.json'; // 確保路徑正確
 
   export default {
     components: {
@@ -19,16 +19,38 @@
       ElOption,
     },
     setup() {
-      const formData = ref({ table: '', field: '', calc: '' });
+      const fieldsConfig = ref([
+        { label: "計算健保費個人負擔", model: "healthPersonal" },
+        { label: "計算健保費公司負擔", model: "healthCompany" },
+        { label: "計算勞保費個人負擔", model: "laborPersonal" },
+        { label: "計算勞保費公司負擔", model: "laborCompany" },
+        { label: "計算勞工退休", model: "laborPension" },
+      ]);
+
+      const formData = ref({
+        table: '', 
+        salary: '', 
+        healthPersonal: '',
+        healthCompany: '',
+        laborPersonal: '',
+        laborCompany: '',
+        laborPension: ''
+      });
       const tableMetaList = ref([]);
       const fieldsMetaList = ref([]);
+      const fieldsCalcMetaList = ref([]);
 
       const updateFields = async () => {
         const tableId = formData.value.table;
+        // 設定薪資欄位的列表
         if (tableId) {
           const table = await bitable.base.getTableById(tableId);
           const fields = await table.getFieldMetaList();
           fieldsMetaList.value  = fields.filter((meta) => meta.type == 2);
+        }
+        // 如果有薪資欄位已設定則處理被計算欄位的列表
+        if (formData.value.salary) {
+          fieldsCalcMetaList.value = fieldsMetaList.value.filter((meta) => meta.id != formData.value.salary);
         }
       };
 
@@ -36,31 +58,58 @@
         const tableId = formData.value.table;
         if (tableId) {
           const table = await bitable.base.getTableById(tableId);
-          const salaryField = formData.value.field;
-          const calcId = formData.value.calc;
+          const salaryField = formData.value.salary;
           const recordIdList = await table.getRecordIdList();
 
           recordIdList.forEach(async (recordId) => {
             let salaryValue = await table.getCellValue(salaryField,recordId);
-            table.setCellValue(calcId,recordId, salaryValue + 2000);
+            
+            const ranges = laborInsuranceRanges.filter((range) => {
+              if (salaryValue > range.min && salaryValue <= range.max) {
+                return range;
+              }
+            })
+
+            if (ranges.length == 0) {
+              table.setCellValue(calcId,recordId, 0);
+            } else {
+              // 定義映射表
+              const mapping = [
+                { key: 'healthPersonal', rangeKey: 'healthPersonal' },
+                { key: 'healthCompany', rangeKey: 'healthCompany' },
+                { key: 'laborPersonal', rangeKey: 'laborPersonal' },
+                { key: 'laborCompany', rangeKey: 'laborCompany' },
+                { key: 'laborPension', rangeKey: 'laborPension' }
+              ];
+
+              // 迴圈處理每個項目
+              mapping.forEach(({ key, rangeKey }) => {
+                if (formData.value[key]) {
+                  table.setCellValue(formData.value[key], recordId, ranges[0][rangeKey]);
+                }
+              });
+            }
+
           })
         }
       }
 
       onMounted(async () => {
-        // alert(JSON.stringify(laborInsuranceRanges))
         const [tableList, selection] = await Promise.all([bitable.base.getTableMetaList(), bitable.base.getSelection()]);
         formData.value.table = selection.tableId;
         tableMetaList.value = tableList;
         const table = await bitable.base.getTableById(selection.tableId);
         const fields = await table.getFieldMetaList();
+        // 一開始先設定薪資欄位的列表, 只有數字型態的欄位可以被設定
         fieldsMetaList.value  = fields.filter((meta) => meta.type == 2);
       });
 
       return {
+        fieldsConfig,
         formData,
-        tableMetaList,
-        fieldsMetaList,
+        tableMetaList, // 資料表列表
+        fieldsMetaList, // 薪資設定欄位列表
+        fieldsCalcMetaList, // 被計算欄位列表
         updateFields,
         calc_salary
       };
@@ -70,72 +119,24 @@
 
 <template>
   <el-form ref="form" class="form" :model="formData" label-position="top">
-    <el-form-item label="开发指南">
-      <a
-        href="https://bytedance.feishu.cn/docx/HazFdSHH9ofRGKx8424cwzLlnZc"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        扩展脚本开发指南
-      </a>
-      、
-      <a
-        href="https://bytedance.feishu.cn/docx/VxhudDXbyo1V7jxAcTbctJQ5nvc"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Base Extension Scripts Guide
-      </a>
-    </el-form-item>
-    <el-form-item label="API">
-      <a
-        href="https://bytedance.feishu.cn/docx/HjCEd1sPzoVnxIxF3LrcKnepnUf"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        扩展脚本API
-      </a>
-      、
-      <a
-        href="https://bytedance.feishu.cn/docx/OPatd1tBZoWogFxKKtmcBZMxnle"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Base Extension Scripts Front-end API
-      </a>
-    </el-form-item>
-    <el-form-item label="选择数据表" size="large">
-        <el-select v-model="formData.table" placeholder="请选择数据表" style="width: 100%" @change="updateFields">
-          <el-option
-            v-for="meta in tableMetaList"
-            :key="meta.id"
-            :label="meta.name"
-            :value="meta.id"
-          />
+    <el-form-item label="請選擇資料表" size="large">
+        <el-select v-model="formData.table" placeholder="請選擇資料表" @change="updateFields">
+          <el-option v-for="meta in tableMetaList" :key="meta.id" :label="meta.name" :value="meta.id"/>
         </el-select>
     </el-form-item>
-    <el-form-item label="底薪欄位" size="large" v-show="formData.table">
-        <el-select v-model="formData.field" placeholder="請選擇底薪欄位" style="width: 100%">
-          <el-option
-            v-for="meta in fieldsMetaList"
-            :key="meta.id"
-            :label="meta.name"
-            :value="meta.id"
-          />
+    <el-form-item label="設定底薪欄位" size="large" v-show="formData.table">
+        <el-select v-model="formData.salary" placeholder="請選擇薪資欄位" @change="updateFields">
+          <el-option v-for="meta in fieldsMetaList" :key="meta.id" :label="meta.name" :value="meta.id"/>
         </el-select>
     </el-form-item>
 
-    <el-form-item label="計算欄位" size="large" v-show="formData.table">
-        <el-select v-model="formData.calc" placeholder="請選擇要放的欄位" style="width: 100%">
-          <el-option
-            v-for="meta in fieldsMetaList"
-            :key="meta.id"
-            :label="meta.name"
-            :value="meta.id"
-          />
+    <el-form-item v-for="field in fieldsConfig" :key="field.model" :label="field.label" size="large" v-show="formData.table">
+        <el-select v-model="formData[field.model]" placeholder="請選擇要放的欄位">
+          <el-option v-for="meta in fieldsMetaList" :key="meta.id" :label="meta.name" :value="meta.id"/>
         </el-select>
     </el-form-item>
-    <el-button type="primary" plain size="large" @click="calc_salary">新增一行记录</el-button>
+
+    <el-button type="primary" plain size="large" @click="calc_salary">開始計算</el-button>
   </el-form>
 </template>
 
@@ -147,5 +148,8 @@
   }
   .form :deep(.el-form-item__content) {
     font-size: 16px;
+  }
+  .el-select {
+    width: 100%;
   }
 </style>
